968703a34f7198327676ad7ac809d355a1b68d13
Riyan Adit Khaerudin <riyanadit31@gmail.com>
2026-02-13 20:41:34 +0700

    Versi terbaru

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
-/** @type {import('next').NextConfig} */
 
 const nextConfig: NextConfig = {
-  experimental: {
-    outputFileTracingIncludes: {
-      '/api/**/*': ['./data/**/*'], // Paksa bawa folder data ke semua API
-    },
-  },
+  
 };
 
 export default nextConfig;
